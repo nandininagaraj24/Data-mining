7508eb189f10fc5beafbf088c57434d659ff6471
Nandini Nagaraj <nandini.nagaraj24@gmail.com>
2015-01-03 02:23:14 -0500

    Create DataminingIF.java

---
+++ DataminingIF.java
@@ -0,0 +1,20 @@
+package com.datamining.constants;
+
+public interface DataminingIF {
+	
+	/*
+	 * /**
+	 *  This is Driver Name 
+	 */
+	//public static final String Driver_name_oracle="com.mysql.jdbc.Driver";
+	
+	public static final String DATABASE_DRIVER="com.mysql.jdbc.Driver"; 
+	
+	public static final String  DATABASE_URL ="jdbc:mysql://localhost:3306/datamining";
+	
+	public static final String  USERNAME="root";
+	
+	public static final String  PASSWORD="ganesha";
+	
+	
+}
