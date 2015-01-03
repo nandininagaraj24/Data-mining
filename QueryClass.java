c7142f132df9ccad37183016c598c30586021953
Nandini Nagaraj <nandini.nagaraj24@gmail.com>
2015-01-03 02:32:08 -0500

    Create QueryClass.java

---
+++ QueryClass.java
@@ -0,0 +1,11 @@
+package com.datamining.visitorinfo;
+
+public class QueryClass 
+{
+	
+		public  static final  String CUSTOMERNAME_QUERY="select customer_name from visitor_info";
+	
+		public  static final  String WEBPAGE_QUERY="select web_page from visitor_info";
+		
+		public  static final  String DATE_QUERY="select date_visit from visitor_info";
+}
